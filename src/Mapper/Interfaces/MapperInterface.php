730a68e3ea9e4bb7afab84f304ddf3475eb72a45
Vitaliy <theyamshikov@yandex.ru>
2021-07-23 14:37:04 +0600

    upd

--- src/Mapper/Interfaces/MapperInterface.php
+++ src/Mapper/Interfaces/MapperInterface.php
@@ -2,10 +2,14 @@
 
 namespace ZnCore\Contract\Mapper\Interfaces;
 
+/**
+ * Interface MapperInterface
+ * @package ZnCore\Contract\Mapper\Interfaces
+ * @deprecated
+ */
 interface MapperInterface
 {
 
-    public function encode(object $entity): array;
-    
-    public function decode(array $row): object;
+//    public function encode(object $entity): array;
+//    public function decode(array $row): object;
 }
