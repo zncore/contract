a540649c390da42ef8c4d1f54331153e74b005b9
Vitaliy <theyamshikov@yandex.ru>
2021-08-09 11:22:29 +0600

    upd

---
+++ src/Domain/Interfaces/ForgeQueryByFilterInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ZnCore\Domain\Interfaces;
+
+use ZnCore\Domain\Libs\Query;
+
+interface ForgeQueryByFilterInterface
+{
+
+    public function forgeQueryByFilter(object $filterModel, Query $query);
+}
