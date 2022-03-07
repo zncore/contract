96b6ef56b924e6e85ca8a0516e7867715f352ca9
Vitaliy <theyamshikov@yandex.ru>
2022-03-07 20:48:05 +0600

    upd

--- src/Common/Exceptions/UnauthorizedException.php
+++ src/Common/Exceptions/UnauthorizedException.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace ZnCore\Contract\User\Exceptions;
+namespace ZnCore\Contract\Common\Exceptions;
+
+DeprecateHelper::softThrow();
 
 use Exception;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use ZnCore\Base\Helpers\DeprecateHelper;
 
+/**
+ * Class UnauthorizedException
+ * @package ZnCore\Contract\Common\Exceptions
+ * @deprecated 
+ */
 class UnauthorizedException extends AccessDeniedException
 {
 
