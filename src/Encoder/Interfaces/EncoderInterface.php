26e3f6c12ba5979c040530df5258a8b324def37f
Vitaliy <theyamshikov@yandex.ru>
2022-06-20 12:13:02 +0600

    upd

--- src/Encoder/Interfaces/EncoderInterface.php
+++ src/Encoder/Interfaces/EncoderInterface.php
@@ -6,6 +6,10 @@ namespace ZnCore\Contract\Encoder\Interfaces;
  * Возможность кодирования/декодирования данных
  * 
  * Другими словами: сериализация/десериализация данных
+ *
+ * @todo наследовать от:
+ * @see \Symfony\Component\Serializer\Encoder\EncoderInterface
+ * @see \Symfony\Component\Serializer\Encoder\DecoderInterface
  */
 interface EncoderInterface
 {
