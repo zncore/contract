be82cdcae63599bbbd52b452783a579b54b53b63
Vitaliy <theyamshikov@yandex.ru>
2022-07-14 11:23:54 +0600

    upd

--- src/User/Exceptions/UnauthorizedException.php
+++ src/User/Exceptions/UnauthorizedException.php
@@ -2,12 +2,13 @@
 
 namespace ZnCore\Contract\User\Exceptions;
 
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+//use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Exception;
 
 /**
  * Не аутентифицированный пользователь
  */
-class UnauthorizedException extends AccessDeniedException
+class UnauthorizedException extends Exception
 {
 
 }
